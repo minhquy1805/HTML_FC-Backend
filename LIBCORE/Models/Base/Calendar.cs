451f8a9cd25922d8f7bb04a651a2575acfdc0439
Nguyen Minh Quy <minhquy073@gmail.com>
2025-05-31 22:08:47 +0700

    handle created at in calendar

--- LIBCORE/Models/Base/Calendar.cs
+++ LIBCORE/Models/Base/Calendar.cs
@@ -41,7 +41,6 @@ namespace LIBCORE.Models
         [Display(Name = "Field3")]
         public string? Field5 { get; set; }
 
-        [RegularExpression(@"^(((((0[13578])|([13578])|(1[02]))[\-\/\s]?((0[1-9])|([1-9])|([1-2][0-9])|(3[01])))|((([469])|(11))[\-\/\s]?((0[1-9])|([1-9])|([1-2][0-9])|(30)))|((02|2)[\-\/\s]?((0[1-9])|([1-9])|([1-2][0-9]))))[\-\/\s]?\d{4})(\s(((0[1-9])|([1-9])|(1[0-2]))\:([0-5][0-9])((\s)|(\:([0-5][0-9])\s))([AM|PM|am|pm]{2,2})))?$", ErrorMessage = "{0} must be a valid date!")]
         [DisplayFormat(DataFormatString = "{0:M/d/yyyy}", ApplyFormatInEditMode = true)]
         [Display(Name = "Created At")]
         public DateTime? CreatedAt { get; set; }
